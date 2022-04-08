2c9d5138c1fc82906734f50e71b24fc964fa0a08
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-04-08 18:39:54 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    const focusEventHandler = () => {
+        console.log('Good!')
+    }
+
+    const blurEventHandler = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    return (<button onFocus={focusEventHandler} onBlur={blurEventHandler}>Eyes on me</button>)
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+    const changeEventHandler = () => {
+        console.log('Entering password...')
+    }
+    return(
+    <input type="password" onChange = {changeEventHandler}/>
+    )}
+
+export default Keypad
